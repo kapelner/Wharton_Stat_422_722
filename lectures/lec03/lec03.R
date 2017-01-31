8bc0a42599468c9c89ef7d896c66f4066c40f0a8
Adam Kapelner <kapelner@gmail.com>
2017-01-31 16:54:54 -0500

    lec 3 slides final

--- lectures/lec03/lec03.R
+++ lectures/lec03/lec03.R
@@ -1,22 +1,4 @@
 
-##########opt design for slope
-plot(0, 0, xlim = c(0, 1), ylim = c(0, 5), type = "n")
-
-beta_0 = 1.5
-beta_1 = 2
-sigma = 0.6
-n = 10
-Nsim = 500
-
-for (r in 1 : Nsim){
-  x = runif(n)
-  #x = seq(0, 1, length.out = n)
-  #x = c(rep(0, 5), rep(1, 5))
-  y = beta_0 + beta_1 * x + rnorm(n, 0, sigma)
-  mod = lm(y ~ x)
-  abline(mod, col = "grey")
-}
-abline(a = beta_0, b = beta_1, col = "green")
 
 
 ############ extrapolation
@@ -44,6 +26,40 @@ yhats = predict(dtree_mod, data.frame(x = xstars))
 points(xstars, yhats, type = "l", col = "brown")
 
 
+##########opt design for slope
+plot(0, 0, xlim = c(0, 1), ylim = c(0, 5), type = "n")
+
+beta_0 = 1.5
+beta_1 = 2
+sigma = 0.6
+n = 10
+Nsim = 500
+
+for (r in 1 : Nsim){
+  x = runif(n)
+  #x = seq(0, 1, length.out = n)
+  #x = c(rep(0, 5), rep(1, 5))
+  y = beta_0 + beta_1 * x + rnorm(n, 0, sigma)
+  mod = lm(y ~ x)
+  abline(mod, col = "grey")
+}
+abline(a = beta_0, b = beta_1, col = "green")
+
+##recall polynomial regression
+n = 100
+sigma = 2
+set.seed(422.722)
+
+x = seq(0, 10, length.out = n)
+f_of_x = 3 + 1.5 * x #true... pretend it's hidden
+e = rnorm(n, 0, sigma)
+y = f_of_x + e
+Xy = data.frame(x, y)
+mod = lm(y ~ poly(x, 20), Xy)
+plot(x, y)
+xstars = seq(min(x), max(x), length.out = 10000)
+yhats = predict(mod, data.frame(x = xstars))
+points(xstars, yhats, type = "l")
 
 
 
@@ -56,27 +72,3 @@ points(xstars, yhats, type = "l", col = "brown")
 
 
 
-
-library(data.table)
-#http://www4.stat.ncsu.edu/~boos/var.select/baseball.html
-X = fread("baseball.csv")
-X
-
-
-train_idx = unique(sample(1 : n, replace = TRUE))
-test_idx = setdiff(1 : n, train_idx)
-ytest = X[test_idx, "salary_thou"]
-
-lm_mod = lm(log(salary_thou) ~ ., X[train_idx, ])
-summary(lm_mod)$r.squared
-
-library(randomForest)
-rf_mod = randomForest(log(salary_thou) ~ ., X)
-
-y_hat = predict(lm_mod, X[test_idx, ])
-cor(y_hat, log(ytest))^2
-
-
-y_hat = predict(rf_mod, X[test_idx, ])
-cor(y_hat, log(ytest))^2
-rf_mod
