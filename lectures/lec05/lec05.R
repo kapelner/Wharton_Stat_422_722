85f68f915eac27474459ab331769275f5ac4f6da
Adam Kapelner <kapelner@gmail.com>
2017-02-13 11:31:56 -0500

    lec5 up until model selection

--- lectures/lec05/lec05.R
+++ lectures/lec05/lec05.R
@@ -1,24 +1,109 @@
+# install.packages("mlr")
+library(mlr) #KILLER NEW PACKAGE
 
-library(data.table)
-#http://www4.stat.ncsu.edu/~boos/var.select/baseball.html
-X = fread("baseball.csv")
-X
+K_FOLDS = 10
+
+#load the data
+X = read.csv("white_wine.csv")
+dim(X)
+#run a simple regression (model A)
+mod = lm(quality ~ ., X)
+summary(mod)
+
+#create
+white_wine_learning_task = makeRegrTask(data = X, target = "quality")
+#make a 10-fold
+rdesc = makeResampleDesc("CV", iters = K_FOLDS)
+r = resample("regr.lm", white_wine_learning_task, rdesc)
+r
+r$pred$data[1 : 500, ]
+nrow(X)
+sqrt(r$aggr)
+#thus we are not overfitting
 
 
-train_idx = unique(sample(1 : n, replace = TRUE))
-test_idx = setdiff(1 : n, train_idx)
-ytest = X[test_idx, "salary_thou"]
+#now let's do the other models (B)
+#first save the polys
+poly_degree = 3
+poly_formula = as.formula( ~ poly(fixed.acidity, poly_degree) + 
+                         poly(volatile.acidity, poly_degree) + 
+                         poly(citric.acid, poly_degree) + 
+                         poly(residual.sugar, poly_degree) + 
+                         poly(chlorides, poly_degree) +
+                         poly(free.sulfur.dioxide, poly_degree) + 
+                         poly(total.sulfur.dioxide, poly_degree) +
+                         poly(density, poly_degree) + 
+                         poly(pH, poly_degree) + 
+                         poly(sulphates, poly_degree) + 
+                         poly(alcohol, poly_degree))
+mod_matrix = data.frame(model.matrix(poly_formula, X))
+mod_matrix[, 1] = X$quality
+names(mod_matrix)[1] = "quality"
 
-lm_mod = lm(log(salary_thou) ~ ., X[train_idx, ])
-summary(lm_mod)$r.squared
+white_wine_learning_task = makeRegrTask(data = mod_matrix, target = "quality")
+#make a 10-fold
+rdesc = makeResampleDesc("CV", iters = K_FOLDS)
+r = resample("regr.lm", white_wine_learning_task, rdesc)
+sqrt(r$aggr)
 
-library(randomForest)
-rf_mod = randomForest(log(salary_thou) ~ ., X)
-
-y_hat = predict(lm_mod, X[test_idx, ])
-cor(y_hat, log(ytest))^2
+install.packages("lasso2")
+library(lasso2)
 
 
-y_hat = predict(rf_mod, X[test_idx, ])
-cor(y_hat, log(ytest))^2
-rf_mod
+##C
+form = merge.formula(as.formula(quality ~ .^2), poly_formula)
+mod_matrix = data.frame(model.matrix(form, X))
+mod_matrix[, 1] = X$quality
+colnames(mod_matrix)[1] = "quality"
+# head(mod_matrix)
+
+white_wine_learning_task = makeRegrTask(data = mod_matrix, target = "quality")
+#make a 10-fold
+rdesc = makeResampleDesc("CV", iters = K_FOLDS)
+r = resample("regr.lm", white_wine_learning_task, rdesc)
+sqrt(r$aggr)
+
+##D
+form = merge.formula(as.formula(quality ~ .^3), poly_formula)
+mod_matrix = data.frame(model.matrix(form, X))
+mod_matrix[, 1] = X$quality
+colnames(mod_matrix)[1] = "quality"
+# head(mod_matrix)
+
+white_wine_learning_task = makeRegrTask(data = mod_matrix, target = "quality")
+#make a 10-fold
+rdesc = makeResampleDesc("CV", iters = K_FOLDS)
+r = resample("regr.lm", white_wine_learning_task, rdesc)
+sqrt(r$aggr)
+
+##E
+form = merge.formula(as.formula(quality ~ .^4), poly_formula)
+mod_matrix = data.frame(model.matrix(form, X))
+mod_matrix[, 1] = X$quality
+colnames(mod_matrix)[1] = "quality"
+# head(mod_matrix)
+
+white_wine_learning_task = makeRegrTask(data = mod_matrix, target = "quality")
+#make a 10-fold
+rdesc = makeResampleDesc("CV", iters = K_FOLDS)
+r = resample("regr.lm", white_wine_learning_task, rdesc)
+sqrt(r$aggr)
+
+##F
+form = merge.formula(as.formula(quality ~ .^11), poly_formula)
+mod_matrix = data.frame(model.matrix(form, X))
+mod_matrix[, 1] = X$quality
+colnames(mod_matrix)[1] = "quality"
+# head(mod_matrix)
+
+white_wine_learning_task = makeRegrTask(data = mod_matrix, target = "quality")
+#make a 10-fold
+rdesc = makeResampleDesc("CV", iters = K_FOLDS)
+r = resample("regr.lm", white_wine_learning_task, rdesc)
+sqrt(r$aggr)
+
+
+
+
+
+
