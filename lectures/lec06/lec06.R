008d7e8ad18b64193c1bb31287a21bdd44c5d779
Adam Kapelner <kapelner@gmail.com>
2017-02-21 12:58:48 -0500

    lec6 more demos

--- lectures/lec06/lec06.R
+++ lectures/lec06/lec06.R
@@ -22,12 +22,35 @@ resample("regr.randomForest", white_wine_task, rdesc, measures = rmse)
 #another example
 X = read.csv("baseball.csv")
 
-lin_mod = lm(salary_thou ~ ., X)
-summary(lin_mod)
 
-rf_mod = randomForest(salary_thou ~ ., X)
-rf_mod
 
 baseball_task = makeRegrTask(data = X, target = "salary_thou")
 r = resample("regr.lm", baseball_task, rdesc, measures = rmse)
 r = resample("regr.randomForest", baseball_task, rdesc, measures = rmse)
+
+rf_mod = randomForest(salary_thou ~ ., X)
+#oos R^2 and RMSE
+rf_mod
+
+#in-sample R^2
+cor(predict(rf_mod, X), X$salary_thou)^2
+#in-sample R^2 is not to be trusted on RF... RF overfits in-sample but does not overfit oos (weird... YES)
+
+#another example
+install.packages("mlbench")
+library(mlbench)
+#Wisconsin Breast Cancer Database
+data(Sonar)
+X = Sonar
+colnames(X)
+
+
+sonar_task = makeClassifTask(data = X, target = "Class")
+r = resample("classif.multinom", sonar_task, rdesc, measures = acc)
+r = resample("classif.randomForest", sonar_task, rdesc, measures = acc)
+
+r = resample(makeLearner("classif.multinom", predict.type = "prob"), sonar_task, rdesc, measures = auc)
+r = resample(makeLearner("classif.randomForest", predict.type = "prob"), sonar_task, rdesc, measures = auc)
+
+
+
